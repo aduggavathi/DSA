fbf0ffd6233c4737a539a71057124031d4c1b131
Abhishek Venkatesh Duggavathi <57829345+aduggavathi@users.noreply.github.com>
2025-11-16 23:40:31 +0530

    Create intersection_of_list.py

---
+++ intersection_of_list.py
@@ -0,0 +1,49 @@
+# Intersection of Two linked list brute force approach.
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        # TERA GENIUS SAFETY CHECK!
+        if not headA or not headA.next or not headB or not headB.next:
+            return None
+            
+        prevA = currA = headA
+        prevB = currB = headB
+        
+        while True:
+            while currA and currB:
+                prevA = currA
+                prevB = currB
+                if prevA is prevB:
+                    return prevA
+                currA = currA.next
+                currB = currB.next
+            
+            if currA:
+                currB = headB
+                prevB = None
+            elif currB:
+                currA = headA
+                prevA = None
+            else:
+                break
+        return None
+
+# Intersection of Two linked list optimized approach.
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if not headA or not headB:
+            return None
+        currA = headA
+        currB = headB
+
+        while currA != currB:
+
+            if currA is None:
+                currA = headB
+            else:
+                currA = currA.next
+
+            if currB is None:
+                currB = headA
+            else:
+                currB = currB.next
+        return currA
